95bfabd8c980fc53529f6e3d7bc58cb919c7fd3e
btssio <btssio@btssio-VirtualBox>
2014-12-08 16:14:40 +0100

    coucou6

--- vue/vueEntete.inc.php
+++ vue/vueEntete.inc.php
@@ -1,2 +1,10 @@
-<br/>
-<h1>Vente de fleurs</h1>
+<header>
+        	<h1 id="margo">Margo</h1> 
+            <h2 id="enseignants">Espace Enseignants</h2>
+			<nav id="navHeader">
+                <ul id="menuHaut">
+                    <li class="menuPHaut" id="compteBtn"><a href="#">Mon Compte</a></li>
+                    <li class="menuPHaut" id="deconnect"><a href="#">Deconnexion</a></li>
+                </ul>
+			</nav>
+        </header>>
