081bb15c19e0f935b7c845db7d099d70e6c0bad6
btssio <btssio@btssio-VirtualBox>
2014-12-08 16:14:11 +0100

    coucou6

--- vue/vueGauche.inc.php
+++ vue/vueGauche.inc.php
@@ -1,12 +1,11 @@
-<ul class="menugauche">
-    <p><b>Menu</b></p>
-    <li><a href="./index.php" >Accueil</a></li>
-    <hr/>
-    <b>Nos produits</b>
-    <li><a href="./index.php?action=afficherTous" >Tous</a></li>
-<?php
-    foreach ($listeCateg as $categ) {
-        echo "<li><a href=\"./index.php?action=afficherUneCateg&id=".$categ['cat_code']."\" >".$categ['cat_libelle']."</a></li>";
-    }
-?>
-</ul>
+<nav id="navGauche">
+            <ul>
+				<h4 id="titreMenuNav">Menu principal<h4>
+                <li class="menuP" id="accueil"><a href="#">Accueil</a></li>
+                <li class="menuP" id="enseignements"><a href="#">Enseignements</a></li>
+                <li class="menuP" id="filieres"><a href="#">Filière</a></li>
+				<li class="menuP" id="classes"><a href="#">Classes</a></li>
+				<li class="menuP" id="eleves"><a href="#">Elèves</a></li>
+            </ul>
+        </nav>
+        
